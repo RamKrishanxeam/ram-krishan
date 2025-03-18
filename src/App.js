6ede2f05e3ab43bc60d8333dcdae0d57b7940bc7
Ram Krishan <xeam.designer7@gmail.com>
2025-03-18 15:15:09 +0530

    project add

--- src/App.js
+++ src/App.js
@@ -3,65 +3,64 @@ import "./index.css";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import HomeBanner from "./components/HomeBanner";
-import ProjectCard from './components/ProjectCard'
-import AboutMe from './components/AboutMe'
-import SkillCard from './components/SkillCard'
-import ContactForm from './components/ContactForm'
-import AnimatedCursor from "react-animated-cursor"
-
+import ProjectCard from "./components/ProjectCard";
+import AboutMe from "./components/AboutMe";
+import SkillCard from "./components/SkillCard";
+import ContactForm from "./components/ContactForm";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   return (
     <>
-    <AnimatedCursor
-      color="#fff"
-      innerSize={8}
-      outerSize={50}
-      innerScale={1}
-      outerScale={2.2}
-      outerAlpha={0}
-      outerStyle={{
-        background: '#ffffff',
-        mixBlendMode: 'exclusion'
-      }}
-      innerStyle={{
-        backgroundColor: '#F94892'
-      }}
-    />
-    <div>
-      <Navbar />
-      <HomeBanner id = "home"/>
-      <ProjectCard
-        id = "project"
-        className = "odd"
-        projectTitle="Wiggles: Pet Care Made Easy"
-        projectDesc="A one-stop solution for pet data management, automated vaccinations, QR-based lost pet recovery, and organizing pet playdates."
-        projectLink="https://github.com/DevanshSahni/Wiggles"
-        deployedProjectLink="https://wiggles.vercel.app"
-        projectImg={require('./images/projectWiggles.jpg')}
+      <AnimatedCursor
+        color="#fff"
+        innerSize={8}
+        outerSize={50}
+        innerScale={1}
+        outerScale={2.2}
+        outerAlpha={0}
+        outerStyle={{
+          background: "#ffffff",
+          mixBlendMode: "exclusion",
+        }}
+        innerStyle={{
+          backgroundColor: "#F94892",
+        }}
       />
+      <div>
+        <Navbar />
+        <HomeBanner id="home" />
+        <ProjectCard
+          id="project"
+          className="odd"
+          projectTitle="Transforming the Flooring Industry with Generative AI"
+          projectDesc="RARA AI, powered by VISUALEZ, revolutionizes the flooring industry by providing advanced generative AI solutions for design, selection, and installation optimization. Improve efficiency and reduce costs while ensuring a personalized and seamless flooring experience."
+          projectLink="https://github.com/YourUsername/RaraAI"
+          deployedProjectLink="https://chatbot-gules-gamma.vercel.app"
+          projectImg={require("./images/projectWiggles.jpg")}
+        />
 
-      <ProjectCard
-        projectTitle="Animated Card: featuring the latest glassmorphism trend"
-        projectDesc="React JS provided me the flexibility needed to create an interactive card with a glass-like effect, which adds a sense of depth and dimension to the user interface"
-        projectLink="https://github.com/Deepajha14/Touch-me-not-Card"
-        deployedProjectLink="https://deepajha14.github.io/Touch-me-not-Card/"
-        projectImg={require('./images/projectGlassmorphism.png')}
-      />
+        <ProjectCard
+          projectTitle="AI-Driven Learning: Smarter Exam Preparation"
+          projectDesc="Transform the way you prepare for competitive and academic exams with AI-driven insights, personalized feedback, and adaptive support tailored to your learning needs."
+          projectLink="https://github.com/YourUsername/AI-Driven-Learning"
+          deployedProjectLink="https://studybot.in/"
+          projectImg={require("./images/projectGlassmorphism.png")}
+        />
 
-      <ProjectCard
-        className = "odd"
-        projectTitle="Rog-free: accessible anytime, anywhere"
-        projectDesc="An approach to target and heal specific health conditions by transforming the living room into a yoga studio"
-        projectLink="https://github.com/Deepajha14/Rog-Free"
-        deployedProjectLink="https://deepajha14.github.io/Rog-Free/"
-        projectImg={require('./images/projectRogfree.png')}
-      />
-      <AboutMe id = 'about' />
-      <SkillCard id = 'skills' />
-      <ContactForm id = 'contact' />
-      <Footer />
-    </div>
+        <ProjectCard
+          className="odd"
+          projectTitle="JobSeeker: Discover 5000+ Job Opportunities"
+          projectDesc="A platform designed to help job seekers find their next career opportunity. Whether you're passionate about startups or looking for roles in UI design, UX research, Android development, or administration, JobSeeker makes job discovery easier."
+          projectLink="https://github.com/Deepajha14/Rog-Free"
+          deployedProjectLink="https://deepajha14.github.io/Rog-Free/"
+          projectImg={require("./images/projectRogfree.png")}
+        />
+        <AboutMe id="about" />
+        <SkillCard id="skills" />
+        <ContactForm id="contact" />
+        <Footer />
+      </div>
     </>
   );
 }
