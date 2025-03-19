5a5fc633a69f6659d6cc67d975b390a6e60d24ba
Ram Krishan <xeam.designer7@gmail.com>
2025-03-19 11:29:46 +0530

    about section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,7 +10,7 @@ export default function AboutMe({ id }) {
         <p>
           Dynamic <span>React Developer</span> with{" "}
           <span>3+ years of experience</span> specializing in{" "}
-          <span>responsive, user-friendly interfaces</span>. Skilled in
+          <span>responsive, user-friendly interfaces</span>. Skilled in{" "}
           <span>
             {" "}
             HTML, CSS, JavaScript, React.js, Next.js, and UI/UX design tools
@@ -18,7 +18,7 @@ export default function AboutMe({ id }) {
           </span>
           . Proficient in <span>state management using Redux</span> and{" "}
           <span>integrating APIs</span>. Experienced in{" "}
-          <span>version control with Git and GitHub</span>. Completed
+          <span>version control with Git and GitHub</span>. Completed{" "}
           <span>advanced courses in web development and React.js</span>. Strong{" "}
           background in developing <span>dynamic applications</span>,
           <span>optimizing performance</span>, and ensuring{" "}
@@ -30,25 +30,23 @@ export default function AboutMe({ id }) {
         </p>
 
         <p>
-          Currently working at <span>Xeam Ventures Pvt Ltd</span>, contributing
+          Currently working at <span>Xeam Ventures Pvt Ltd</span>, contributing{" "}
           to <span>scalable web solutions</span> and collaborating in{" "}
           <span>agile teams</span>. Actively involved in enhancing{" "}
           <span>application performance</span> through{" "}
-          <span>code optimization and lazy loading techniques</span>.
+          <span>code optimization and lazy loading techniques</span>.{" "}
           Experienced in <span>debugging complex issues</span> and delivering{" "}
-          <span>seamless user experiences</span>
-          across devices. Skilled in translating <span>
-            Figma designs
-          </span> into <span>pixel-perfect, interactive interfaces</span>.
-          Regularly participate in <span>code reviews</span> to ensure{" "}
+          <span>seamless user experiences</span> across devices. Skilled in
+          translating <span>Figma designs</span> into{" "}
+          <span>pixel-perfect, interactive interfaces</span>. Regularly{" "}
+          participate in <span>code reviews</span> to ensure{" "}
           <span>quality and consistency</span> in team projects. Proficient in{" "}
-          using <span>TypeScript</span> to enhance
+          using <span>TypeScript</span> to enhance{" "}
           <span>code reliability and maintainability</span>. Committed to{" "}
           staying updated with the latest industry trends, including{" "}
           <span>React hooks and functional components</span>. Demonstrated{" "}
-          ability to
-          <span>mentor junior developers</span> and share knowledge effectively
-          within the team.
+          ability to <span>mentor junior developers</span> and share knowledge
+          effectively within the team.
         </p>
       </div>
     </div>
